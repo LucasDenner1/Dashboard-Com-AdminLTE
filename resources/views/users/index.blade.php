ed11030952867e28ad66dfca8b5396d276f1f57d
Denner <lucas.denner14@hotmail.com>
2025-03-05 18:11:51 -0300

    Funcionalidade de criar usuários

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,18 +2,40 @@
 
 @section('page-title', 'Usuários')
 
+@section('page-actions')
+    <a href="{{ route('users.create') }}" class="btn btn-primary">Adicionar</a>
+@endsection
+
 @section('content')
+
+@session('status')
+        <div class="alert alert-success" role="alert" >
+            {{ $value }}
+        </div>
+@endsession
 
 <table class="table">
     <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Ação</th>
+        </tr>
+    </thead>
+    <tbody>
         @foreach ($users as $user)
         <tr>
           <th scope="col">{{ $user->id }}</th>
           <th scope="col">{{ $user->name }}</th>
           <th scope="col">{{ $user->email }}</th>
+          <td>
+            <a href="" class="btn btn-primary btn-sm">Editar</a>
+            <a href="" class="btn btn-danger btn-sm">Excluir</a>
+          </td>
         </tr>
         @endforeach
-    </thead>
+    </tbody>
 </table>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/', function () {
         return view('home');
     })->name('home');
-});
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users/create', [UserController::class, 'store'])->name('users.store');
+
+});
