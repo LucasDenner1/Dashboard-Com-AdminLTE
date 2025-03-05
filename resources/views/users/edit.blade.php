8514ff6b3e14ecb13081bd6abc480a71ba729093
Denner <lucas.denner14@hotmail.com>
2025-03-05 20:34:29 -0300

    modularizando página de editar usuário

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -10,36 +10,5 @@
         </div>
 @endsession
 
-<form action="{{ route('users.update', $user->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Name</label>
-      <input type="name" name="name" class="form-control @error('name') is-invalid @enderror" id="exampleInputEmail1" value="{{ old('name') ?? $user->name }}" aria-describedby="emailHelp">
-        @error('name')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-        @enderror
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email</label>
-        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail1" value="{{ old('email') ?? $user->email }}" aria-describedby="emailHelp">
-        @error('email')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Password</label>
-        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
-        @error('password')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-        @enderror
-      </div>
-    <button type="submit" class="btn btn-primary">Editar</button>
-  </form>
+@include('users.parts.base-details')
 @endsection
