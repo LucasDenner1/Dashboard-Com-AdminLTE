f607ccc69854d148f4e15211b241ab037f6e8647
Denner <lucas.denner14@hotmail.com>
2025-03-06 22:49:45 -0300

    Deixando a interface mais apresentável

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -14,18 +14,21 @@
         </div>
 @endsession
 
-<form action="{{ route('users.index') }}" method="GET" class="mb-3" style="width: 300px">
-    <div class="input-group">
-        <input type="text" name="keyword" class="form-control" placeholder="Pesquise por seu nome ou email" value="{{ request()?->keyword }}">
-        <button type="submit" class="btn btn-primary">Pesquisar</button>
-    </div>
-</form>
+<div class="d-flex justify-content-center p-2">
+    <form action="{{ route('users.index') }}" method="GET" class="mb-3 w-100 w-md-50 w-lg-25">
+        <div class="input-group">
+            <input type="text" name="keyword" class="form-control" placeholder="Buscar por nome ou e-mail..." value="{{ request()?->keyword }}">
+            <button type="submit" class="btn btn-primary">Pesquisar</button>
+        </div>
+    </form>
+</div>
 
-<table class="table">
-    <thead>
+
+<table class="table table-striped table-bordered align-middle">
+    <thead class="text-center">
         <tr>
             <th scope="col">#</th>
-            <th scope="col">Name</th>
+            <th scope="col">Nome</th>
             <th scope="col">Email</th>
             <th scope="col">Ação</th>
         </tr>
@@ -33,21 +36,24 @@
     <tbody>
         @foreach ($users as $user)
         <tr>
-          <th scope="col">{{ $user->id }}</th>
-          <th scope="col">{{ $user->name }}</th>
-          <th scope="col">{{ $user->email }}</th>
-          <td>
-            <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Editar</a>
-            <form action="{{ route('user.destroy', $user->id) }}" method="POST">
-                @method('DELETE')
-                @csrf
-                <button type="submit" class="btn btn-danger btn-sm" >Excluir</button>
-            </form>
-          </td>
+            <td class="text-center">{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td class="text-center">
+                <div class="d-flex justify-content-center gap-2">
+                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Editar</a>
+                    <form action="{{ route('user.destroy', $user->id) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-danger btn-sm">Excluir</button>
+                    </form>
+                </div>
+            </td>
         </tr>
         @endforeach
     </tbody>
 </table>
+
 
 {{ $users->links() }}
 @endsection
